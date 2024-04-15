234855709ff8c98c39ce23b67d9013af8acab15a
anubhavnashtech <152603599+anubhavnashtech@users.noreply.github.com>
2024-04-15 21:16:47 +0530

    Update ServiceResponse.cs

--- Model/ServiceResponse.cs
+++ Model/ServiceResponse.cs
@@ -10,7 +10,7 @@ namespace rpgAPI.Model
 
         public T? Data { get; set; }
 
-        public bool Success { get; set; }
+        public bool Success { get; set; } = true;
 
         public string Message { get; set; } = string.Empty;
         
